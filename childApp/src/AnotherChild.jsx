67bbabed4157bcf42cccb07c54b840f32e762a57
ammrit2312 <amriteshc101@icloud.com>
2024-05-06 02:58:03 +0530

    export-router directly from child

--- childApp/src/AnotherChild.jsx
+++ childApp/src/AnotherChild.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AnotherChild = () => {
-  return <div>AnotherChild Component</div>;
+let navigate = useNavigate();
+
+  return (
+    <div>
+      AnotherChild Component
+      <br />
+      <button onClick={() => navigate("/")}>Checking</button>
+    </div>
+  );
 };
 
 export default AnotherChild;
--- parentApp/src/routes.jsx
+++ parentApp/src/routes.jsx
@@ -1,26 +1,32 @@
 import { useRoutes } from "react-router-dom";
 import Home from "./Home";
 import Another from "./Another";
-import { childRoutes } from "childApp/ChildRouter";
+import { ChildRouter } from "childApp/ChildRouter";
 // import Child from "childApp/Child";
 // import AnotherChild from "childApp/AnotherChild";
 
 const MainRouter = () => {
-  const finalRoute = childRoutes.map((route) => {
-    let newPath = route.path.split("/");
-    newPath[0] = "/child";
-    newPath = newPath.join("/");
-    route.path = newPath;
-    return route;
-  });
+  // const finalRoute = childRoutes.map((route) => {
+  //   let newPath = route.path.split("/");
+  //   newPath[0] = "/child";
+  //   newPath = newPath.join("/");
+  //   route.path = newPath;
+  //   return route;
+  // });
 
-  // console.log("Checking ##", finalRoute);
+  console.log("Checking ##", useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/another", element: <Another /> },
+    // { path: "/child", element: <Child /> },
+    // { path: "/child/anotherChild", element: <AnotherChild /> },
+    { path: "/child", element: <ChildRouter /> },
+  ]));
   return useRoutes([
     { path: "/", element: <Home /> },
     { path: "/another", element: <Another /> },
     // { path: "/child", element: <Child /> },
     // { path: "/child/anotherChild", element: <AnotherChild /> },
-    ...finalRoute,
+    { path: "/child/*", element: <ChildRouter /> },
   ]);
 };
 
